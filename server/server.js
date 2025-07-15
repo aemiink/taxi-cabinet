12b39f014121a2e783766344a61dbd01de0ee54b
Emin <102097443+aemiink@users.noreply.github.com>
2025-07-15 16:54:19 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "https://taxicabinet.com",
-  "https://your-railway-project.up.railway.app" // railway domainin buraya eklenmeli
+  "https://taxi-cabinet-production.up.railway.app" // railway domainin buraya eklenmeli
 ];
 
 app.use(
